5a4682d03825dab0c9deaea6eeca27ef669a88bf
reabaes <baes.rea@gmail.com>
2024-10-15 01:58:23 +0800

    user_dashboard

--- web/login.php
+++ web/login.php
@@ -41,9 +41,14 @@
                 <div class="login-form">
                     <h2>Log In</h2>
                     <form action="process_login.php" method="post">
+                         <div class="input-group">
+                            <input id="username" name="roomnumber" placeholder="Room Number" required>
+                        </div>
+
                         <div class="input-group">
                             <input type="email" id="username" name="username" placeholder="Username/Email" required>
                         </div>
+
                         <div class="input-group">
                             <input type="password" id="password" name="password" placeholder="Password" required>
                         </div>
